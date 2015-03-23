71752fcd4945f92428940b6c77f3df9e90d81aa8
Marco30 <mv222fp@student.lnu.se>
2015-03-23 14:02:49 +0100

    fixa ok klar

--- APM/APM/Model/KontaktTyp.cs
+++ APM/APM/Model/KontaktTyp.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace APM.Model
+namespace APM.Model// Marco Villegas
 {
     public class KontaktTyp
     {
